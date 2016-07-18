323633c5b9d3fff3aee2891d2c7ec165983f2764
Steven Cheng <chengliangchengliang888@gmail.com>
2016-07-18 17:32:35 +0800

    研究batchInsert API对多类型Record的支持.

--- src/main/java/example/dao/layer/rdbms/orderLog/impl/OrderLogDaoImpl.java
+++ src/main/java/example/dao/layer/rdbms/orderLog/impl/OrderLogDaoImpl.java
@@ -6,7 +6,6 @@ import example.dao.layer.rdbms.orderLog.OrderLogDao;
 import example.framework.layer.log.LogHelper;
 import example.framework.layer.rdbms.transaction.JooqTransactionFactory;
 import example.framework.layer.util.Utils;
-import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -25,6 +24,36 @@ public class OrderLogDaoImpl implements OrderLogDao {
 
     @Override
     public int add(int orderId, Status status) {
+//        LogHelper.info("---------------------------------------------------");
+//        //混合类型的insert对象，在内部进行按类型分组后，每组进行一次batchInsert
+//        List list = new ArrayList<>();
+//        OrderLogRecord orderLogRecord = jooq.context().newRecord(ORDER_LOG);
+//        orderLogRecord.setOrderId(orderId);
+//        orderLogRecord.setStatus(status.getValue());
+//        orderLogRecord.setChangeTime(Utils.nowTime());
+//
+//
+//        OrderLogRecord orderLogRecord2 = jooq.context().newRecord(ORDER_LOG);
+//        orderLogRecord2.setOrderId(orderId);
+//        orderLogRecord2.setStatus((byte) 5);
+//        orderLogRecord2.setChangeTime(Utils.nowTime());
+//
+//        OrderRecord orderRecord = jooq.context().newRecord(ORDER);
+//        orderRecord.setUid(15896);
+//        orderRecord.setAmout(14736l);
+//        orderRecord.setOrderTime(Utils.nowTime());
+//        orderRecord.setRemark("232323232323");
+//        orderRecord.setStatus((byte) 3);
+//
+//        list.add(orderLogRecord);
+//        list.add(orderLogRecord2);
+//        list.add(orderRecord);
+//
+//        int[] result = jooq.context().batchInsert(list).execute();
+//        LogHelper.info("*************************************************************");
+//        return result.length;
+
+
         OrderLogRecord orderLogRecord = jooq.context().newRecord(ORDER_LOG);
         orderLogRecord.setOrderId(orderId);
         orderLogRecord.setStatus(status.getValue());
--- src/main/java/example/framework/layer/rdbms/transaction/JooqTransactionFactory.java
+++ src/main/java/example/framework/layer/rdbms/transaction/JooqTransactionFactory.java
@@ -3,7 +3,6 @@ package example.framework.layer.rdbms.transaction;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
-import org.jooq.conf.Settings;
 import org.jooq.impl.DSL;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultExecuteListenerProvider;
@@ -18,28 +17,36 @@ import javax.sql.DataSource;
  */
 public class JooqTransactionFactory {
 
-    private final Configuration config = new DefaultConfiguration();
+    //private final static Configuration config = new DefaultConfiguration();
+    private Configuration config;
+    private DSLContext context;
 
-    public JooqTransactionFactory(SpringTransactionProvider springTransactionProvider,SpringExceptionTranslationExecuteListener springExceptionTranslationExecuteListener,DataSource dataSource) {
-        config.set(new SpringTransactionConnectionProvider(dataSource)).
-                set(SQLDialect.MYSQL).
-                set(springTransactionProvider).
-                set(new DefaultExecuteListenerProvider(springExceptionTranslationExecuteListener));
+    public JooqTransactionFactory(SpringTransactionProvider springTransactionProvider, SpringExceptionTranslationExecuteListener springExceptionTranslationExecuteListener, DataSource dataSource) {
+        if (config == null) {
+            config = new DefaultConfiguration();
+            config.set(new SpringTransactionConnectionProvider(dataSource)).
+                    set(SQLDialect.MYSQL).
+                    set(springTransactionProvider).
+                    set(new DefaultExecuteListenerProvider(springExceptionTranslationExecuteListener));
+        }
     }
 
-    public JooqTransactionFactory(DataSource ds, SQLDialect dialect) {
-        this(ds, dialect, new Settings().withRenderSchema(false));
-    }
-
-    public JooqTransactionFactory(DataSource ds, SQLDialect dialect,
-                                  Settings settings) {
-        config.set(new SpringTransactionConnectionProvider(ds)).set(dialect).set(
-                settings).set(
-                new DefaultExecuteListenerProvider(
-                        new SpringExceptionTranslationExecuteListener()));
-    }
+//    public JooqTransactionFactory(DataSource ds, SQLDialect dialect) {
+//        this(ds, dialect, new Settings().withRenderSchema(false));
+//    }
+//
+//    public JooqTransactionFactory(DataSource ds, SQLDialect dialect,
+//                                  Settings settings) {
+//        config.set(new SpringTransactionConnectionProvider(ds)).set(dialect).set(
+//                settings).set(
+//                new DefaultExecuteListenerProvider(
+//                        new SpringExceptionTranslationExecuteListener()));
+//    }
 
     public DSLContext context() {
-        return DSL.using(config);
+        if (context == null) {
+            context = DSL.using(config);
+        }
+        return context;
     }
 }
