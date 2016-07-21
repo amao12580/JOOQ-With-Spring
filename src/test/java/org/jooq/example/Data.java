615b26de0a70b79797542067d0f9e2fc50310c8f
Steven Cheng <chengliangchengliang888@gmail.com>
2016-07-21 14:44:07 +0800

    code optimization

--- src/test/java/org/jooq/example/Data.java
+++ src/test/java/org/jooq/example/Data.java
@@ -6,38 +6,14 @@ package org.jooq.example;
  * Date:2016/7/15
  * Time:14:22
  */
-public class Data implements Comparable {
+@lombok.Data
+public class Data implements Comparable<Data> {
     String title;
     int smonth;
     int sdate;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getSmonth() {
-        return smonth;
-    }
-
-    public void setSmonth(int smonth) {
-        this.smonth = smonth;
-    }
-
-    public int getSdate() {
-        return sdate;
-    }
-
-    public void setSdate(int sdate) {
-        this.sdate = sdate;
-    }
-
     @Override
-    public int compareTo(Object o) {
-        Data data = (Data) o;
+    public int compareTo(Data data) {
         if (this.getUniqueCode() == data.getUniqueCode()) {
             return 0;
         }
